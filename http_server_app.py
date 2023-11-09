8d4e724804c2f8153d145b058c9a7ff0275ae02a
Bhaskar <bhskr150@gmail.com>
2023-11-09 08:36:27 +0530

    python code

---
+++ http_server_app.py
@@ -0,0 +1,19 @@
+import http.server
+from prometheus_client  import start_http_server, Counter
+
+Request_count = Counter("app_request_count","Number of requessts received")
+
+class HandleRequests(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        Request_count.inc()
+        self.send_response(200)
+        self.send_header("Content-type","text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>First python Application</title></head></html>"))
+        self.wfile.close
+
+if __name__ == "__main__":
+    start_http_server(5001)
+    server = http.server.HTTPServer(('localhost',5000), HandleRequests)
+    server.serve_forever()
